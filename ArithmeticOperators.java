1adaa874c1c80bbb7b94a75124de3237a7154da9
Tejasree-S27 <stejasree348@gmail.com>
2024-12-03 10:09:30 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ArithmeticOperators
+{
+    public static void main(String args[])
+    {
+        int a=10,b=5,c;
+        c=a+b;
+        System.out.println("a+b="+c);
+        c=a-b;
+        System.out.println("a-b="+c);
+        c=a*b;
+        System.out.println("a*b="+c);
+        c=a/b;
+        System.out.println("a/b="+c);
+        c=a%b;
+        System.out.println("a%b="+c);
+    }
+}
