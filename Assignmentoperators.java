57a2391cd627b5eff67addb32b1f071ff44fd4d5
Tejasree-S27 <stejasree348@gmail.com>
2024-12-03 10:58:02 +0530

    Add files via upload

---
+++ Assignmentoperators.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Assignmentoperators 
+{
+    public static void main(String args[])
+    {
+       int a=34;
+       System.out.println(a+=30);
+       System.out.println(a-=5);
+       System.out.println(a*=3);
+       System.out.println(a/=2);
+       System.out.println(a%=2);
+    }
+}
