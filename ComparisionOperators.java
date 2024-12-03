0c6777d4e77b68d34725bcc026808c7bd4d2e22b
Tejasree-S27 <stejasree348@gmail.com>
2024-12-03 10:20:27 +0530

    Add files via upload

---
+++ ComparisionOperators.java
@@ -0,0 +1,15 @@
+public class ComparisionOperators 
+{
+    public static void main(String args[])
+    {
+        int a=34;
+        int b=43;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+    }
+    
+}
