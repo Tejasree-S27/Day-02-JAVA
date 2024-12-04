2155cba422ac1cef037fd14b5fee77e48065a8f4
Tejasree-S27 <stejasree348@gmail.com>
2024-12-04 23:05:49 +0530

    Add files via upload

---
+++ LeftShift.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class LeftShift
+{
+    public static void main(String args[])
+    {
+        Scanner n=new Scanner (System.in);
+        int a=n.nextInt();
+        int b=n.nextInt();
+        System.out.println(a<<b);
+    }
+}
